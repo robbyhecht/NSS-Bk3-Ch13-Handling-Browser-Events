f386ecfb39e48dd677c709181e71c7856b3ca6e6
robbyhecht <robbyhecht@gmail.com>
2018-10-30 21:44:52 -0500

    completed activate and deactivate all buttons exercise

--- wonder powers/powers.js
+++ wonder powers/powers.js
@@ -1,12 +1,17 @@
-// flight function
+// FLIGHT FUNCTION
+
+// make a variable for the selected button
 let activateFlight = document.querySelector("#activate-flight")
+// function creates variable for dom location and then replaces class of this location
 let flightHandlerFunction = () => {
   let flight = document.getElementById("flight")
   flight.classList.replace("disabled", "enabled")
 }
+// event listener on the button runs the class-changing function when clicked
 activateFlight.addEventListener("click", flightHandlerFunction)
 
-// mindreading function
+
+// MINDREADING FUNCTION
 let activateMindreading = document.querySelector("#activate-mindreading")
 let mindreadingFunction = () => {
   let mindreading = document.getElementById("mindreading")
@@ -14,7 +19,8 @@ let mindreadingFunction = () => {
 }
 activateMindreading.addEventListener("click", mindreadingFunction)
 
-// xray function
+
+// XRAY FUNCTION
 let activateXray = document.querySelector("#activate-xray")
 let xrayFunction = () => {
   let xray = document.getElementById("xray")
